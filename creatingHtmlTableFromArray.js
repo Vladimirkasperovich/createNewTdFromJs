0d7c50fbdfec39433d67d084a45e10ae05409fa6
Vladimirkasperovich <vova.kasper.boy@gmail.com>
2022-08-25 15:56:54 +0300

    create commit
    create commit

---
+++ creatingHtmlTableFromArray.js
@@ -0,0 +1,23 @@
+//Модифицируйте предыдущую задачу так, чтобы в ячейках таблицы были записаны четные числа в промежутке от 2 до 50.
+  /* let table = document.querySelector('#table');
+
+   for (let i = 0; i < 5; i++) {
+     let tr = document.createElement('tr');
+      const initValue = i * 10
+
+       for (let k = initValue; k < 10 + initValue; k++) {
+         if (k % 2 === 0) {
+           let td = document.createElement('td');
+
+           td.innerHTML = k
+           tr.appendChild(td);
+         }
+       }
+
+     table.appendChild(tr);
+   }*/ 
+
+
+
+   //
+ 
